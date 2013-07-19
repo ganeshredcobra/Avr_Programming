2969c0ccef8089d529160c8562301b0d73604a01
ganesh <ganesh@ganesh.(none)>
2013-07-20 00:18:40 +0530

    Blink led using switch

---
+++ Button/button_switch.c
@@ -0,0 +1,38 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  button_switch.c
+ *
+ *    Description:  Change delay with respect to button press
+ *
+ *        Version:  1.0
+ *        Created:  Friday 19 July 2013 01:03:40  IST
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Ganesh H (), ganeshredcobra@gmail.com
+ *        Company:  space-kerala.org
+ *
+ * =====================================================================================
+ */
+
+#include <avr/io.h>
+#include <avr/delay.h>
+
+int main()
+{
+	DDRB |= 1<<PINB0;
+	DDRB &= ~(1<<PINB1);
+	PORTB |= 1<<PINB1;
+	while(1)
+	{
+		if(bit_is_clear(PINB,1))
+		{
+			PORTB |= 1<<PINB0;
+		}
+		else
+		{
+			PORTB &= ~(1<<PINB0);
+		}
+	}
+}
